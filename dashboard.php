cbbaf9139817e53cc03c1d4fdadbd0e9a67b8aab
riskimidiw <riskimidiw@gmail.com>
2018-06-03 20:40:27 +0700

    tampilan awal

---
+++ dashboard.php
@@ -0,0 +1,13 @@
+<?php include "views/header-white.php"; ?>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-2 bg-dark sidebar">
+            <p>home</p>
+            <p>blog</p>
+        </div>
+        <div class="col-8">
+
+        </div>
+    </div>
+</div>
