2a1e35aa9e9206af0bf44f19886d4d50294fce17
riskimidiw <riskimidiw@gmail.com>
2018-06-03 20:40:27 +0700

    tampilan awal

---
+++ views/header-white.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="theme-color" content="#000">
+    <link rel="icon" href="public/img/itc.png">
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="stylesheet" href="public/css/query.css">
+    <script src="public/js/jquery.min.js"></script>
+    <title>ITC UPNYK</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+        <div class="container">
+            <a href="/itc" class="navbar-brand font-weight-bold"><img src="public/img/itc.png" alt="logo" class="mr-3">ITC UPNYK</a>
+            <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ml-auto">
+                    <a class="nav-item nav-link font-weight-bold" href="/itc/login.php">Login</a>
+                    <a class="nav-item nav-link font-weight-bold" href="/itc/signup.php">Sign Up</a>
+                </div>
+            </div>
+        </div>
+    </nav>
