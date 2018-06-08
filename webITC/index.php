d788ab1bf29554ac5d349799c9b59a225d2b837f
hardzal <rizaldoeta98@gmail.com>
2018-06-08 09:11:46 +0700

    Update Structure File

---
+++ webITC/index.php
@@ -0,0 +1,124 @@
+<?php
+    require_once "views/header.php";
+?>
+
+<div class="head mb-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h2 class="display-4 font-weight-bold mb-4">Information Technology Club</h2>
+                <h3 class="text-light mb-4">Sharing is the <strong>key</strong> to be great <strong>developer</strong></h3>
+                <a href="#pelatihan" id="scroll-pelatihan" class="btn btn-success px-4 font-weight-bold">PELATIHAN</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container text-center pb-md-5 pt-lg-5 mb-5 divisi">
+    <h1 class="font-weight-bold mb-5 pb-2 text-uppercase"><ion-icon name="contacts"></ion-icon> Divisi</h1>
+    <div class="pt-4">
+        <div class="row rounded p-3 mb-5">
+            <div class="col-md-4">
+                <div class="img-thumbnail mb-4 p-3 border-0">
+                    <h3 class="font-weight-bold pb-3 border-bottom"><ion-icon name="code"></ion-icon> Litbang Intern</h3>
+                    <p class="pt-2">Melakukan pelatihan dan meningkatkan skill SDM di Teknik Informatika</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="img-thumbnail mb-4 p-3 border-0">
+                    <h3 class="font-weight-bold pb-3 border-bottom"><ion-icon name="megaphone"></ion-icon> Humas</h3>
+                    <p class="pt-2">Melakukan penyebaran informasi terkait ITC dan dokumentasi pelatihan</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="img-thumbnail p-3 border-0">
+                    <h3 class="font-weight-bold pb-3 border-bottom"><ion-icon name="trophy"></ion-icon> Litbang Extern</h3>
+                    <p class="pt-2">Fokus terhadap lomba IT khususnya Competitive Programming</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="pt-md-5 pb-md-5 mb-md-5" id="pelatihan">
+    <div class="container pt-5 pb-5 text-center">
+        <h1 class="font-weight-bold pb-2 mb-5 text-uppercase text-white"><ion-icon name="easel"></ion-icon> Pelatihan</h1>
+        <div class="row">
+            <div class="col-md-6 m-auto">
+                <div class="img-thumbnail p-3 border-0">
+                    <h2 class="font-weight-bold pb-3 mb-4">Web Development</h2>
+                    <ion-icon name="checkmark" class="text-success"></ion-icon> HTML, CSS, Javacript <br>
+                    <ion-icon name="checkmark" class="text-success"></ion-icon> Bootstrap 4 <br>
+                    <ion-icon name="checkmark" class="text-success"></ion-icon> PHP <br>
+                    <ion-icon name="checkmark" class="text-success"></ion-icon> MySql <br>
+                    <ion-icon name="checkmark" class="text-success"></ion-icon> Laravel <br>
+                    <a href="#" class="btn btn-success mt-4 font-weight-bold">Daftar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container pb-5 mb-5 pt-5 blog text-center">
+    <h1 class="font-weight-bold pb-2 mb-5 text-uppercase"><ion-icon name="paper"></ion-icon> Blog</h1>
+    <div class="row text-left">
+        <div class="col-md-6 mb-3">
+            <div class="img-thumbnail border-0">
+                <div class="row">
+                    <div class="col-md-5 mb-2">
+                        <img src="https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?cs=srgb&dl=coding-computer-data-577585.jpg&fm=jpg" alt="">
+                    </div>
+                    <div class="col">
+                        <h4 class="mb-2 font-weight-bold">Judul Blog</h4>
+                        <p>Ini adalah blog asjdasjd ajsdashdhahsd ahsdhhash hasdhhahsdj</p>
+                        <a href="/itc/blog.php" class="btn btn-primary">Lihat</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 mb-3">
+            <div class="img-thumbnail border-0">
+                <div class="row">
+                    <div class="col-md-5 mb-2">
+                        <img src="https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?cs=srgb&dl=coding-computer-data-577585.jpg&fm=jpg" alt="">
+                    </div>
+                    <div class="col">
+                        <h4 class="mb-2 font-weight-bold">Judul Blog</h4>
+                        <p>Ini adalah blog asjdasjd ajsdashdhahsd ahsdhhash hasdhhahsdj</p>
+                        <a href="/itc/blog.php" class="btn btn-primary">Lihat</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 mb-3">
+            <div class="img-thumbnail border-0">
+                <div class="row">
+                    <div class="col-md-5 mb-2">
+                        <img src="https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?cs=srgb&dl=coding-computer-data-577585.jpg&fm=jpg" alt="">
+                    </div>
+                    <div class="col">
+                        <h4 class="mb-2 font-weight-bold">Judul Blog</h4>
+                        <p>Ini adalah blog asjdasjd ajsdashdhahsd ahsdhhash hasdhhahsdj</p>
+                        <a href="/itc/blog.php" class="btn btn-primary">Lihat</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 mb-3">
+            <div class="img-thumbnail border-0">
+                <div class="row">
+                    <div class="col-md-5 mb-2">
+                        <img src="https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?cs=srgb&dl=coding-computer-data-577585.jpg&fm=jpg" alt="">
+                    </div>
+                    <div class="col">
+                        <h4 class="mb-2 font-weight-bold">Judul Blog</h4>
+                        <p>Ini adalah blog asjdasjd ajsdashdhahsd ahsdhhash hasdhhahsdj</p>
+                        <a href="/itc/blog.php" class="btn btn-primary">Lihat</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include "views/footer.php"; ?>
